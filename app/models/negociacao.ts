368be03a17a01d2feeeb07e93728a987863eaf19
ThiagoRibas138 <thiago-ribas@outlook.com>
2022-02-28 16:13:48 -0300

    Inicio do projeto Negociação

---
+++ app/models/negociacao.ts
@@ -0,0 +1,20 @@
+export class Negociacao {
+    
+constructor(
+    private _data: Date, 
+    public readonly quantidade: number, 
+    public readonly valor: number){}
+
+    
+
+    get volume(): number{
+        return this.quantidade * this.valor;
+    }
+
+    get data(): Date{
+        const data = new Date(this._data.getTime());
+        return this._data;
+    }
+
+}
+
