d54593efc7b4d442aeb970da4f313c771abd5e4b
ThiagoRibas138 <thiago-ribas@outlook.com>
2022-03-30 17:33:24 -0300

    Alteração de pastas para melhor aplicação com o serv

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,8 +1,9 @@
-import { DiasDaSemana } from "../enums/dias-da-semana.js";
-import { Negociacao } from "../models/negociacao.js";
-import { Negociacoes } from "../models/negociacoes.js";
-import { MensagemView } from "../views/mensagem-view.js";
-import { NegociacoesView } from "../views/negociacoes-view.js";
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
+import { NegociacoesView } from '../views/negociacoes-view.js';
+
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
